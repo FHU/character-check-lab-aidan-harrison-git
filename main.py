fb092f29bf1ff98caffe5fbba2ababfda491ef49
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-02-01 12:40:05 -0600

    main.py
    trying to grade...once again...

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     print(check_character('happy birthday 2 you', 15))
     print(check_character('happy birthday!', 14))
 
-# try autograding again
+# try autograding againn
